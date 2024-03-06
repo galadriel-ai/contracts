99063078902eed4e17063473022e143ff9e9e17a
kristjanpeterson <kristjan@nftport.xyz>
2024-03-06 17:39:51 +0200

    Move to hardhat

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,6 +2,10 @@ import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 require('dotenv').config()
+const seiDevnet = []
+if (process.env.PRIVATE_KEY_SEI_DEVNET) {
+  seiDevnet.push(process.env.PRIVATE_KEY_SEI_DEVNET)
+}
 const localhostPrivateKeys = []
 if (process.env.PRIVATE_KEY_LOCALHOST) {
   localhostPrivateKeys.push(process.env.PRIVATE_KEY_LOCALHOST)
@@ -10,6 +14,11 @@ if (process.env.PRIVATE_KEY_LOCALHOST) {
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
+    devnet: {
+      chainId: 713715,
+      url: "https://evm-rpc.arctic-1.seinetwork.io",
+      accounts: seiDevnet,
+    },
     // TODO: add SEI etc
     hardhat: {
       chainId: 1337,
@@ -17,7 +26,7 @@ const config: HardhatUserConfig = {
     localhost: {
       chainId: 1337,
       url: "http://127.0.0.1:8545",
-      accounts: localhostPrivateKeys
+      accounts: localhostPrivateKeys,
     }
   },
 };
