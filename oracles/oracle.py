16bacf6ed2de74abdc54a5d3dd990c3223301e3e
kgrofelnik <kresimir.grofelnik@gmail.com>
2024-03-08 12:39:55 +0100

    Cleanup

--- oracles/oracle.py
+++ oracles/oracle.py
@@ -1,12 +1,12 @@
 import asyncio
 from src.repositories.oracle_repository import OracleRepository
-from src.llm import generate_response_use_case
+from src.domain.llm import generate_response_use_case
 
 repository = OracleRepository()
 
 
 async def _answer_unanswered_prompts():
-    prompts = repository.get_unanswered_prompts()
+    prompts = await repository.get_unanswered_prompts()
     for prompt in prompts:
         try:
             response = await generate_response_use_case(
@@ -14,20 +14,19 @@ async def _answer_unanswered_prompts():
             )
             if response:
                 prompt.response = response
-                repository.send_response(prompt, response)
+                await repository.send_response(prompt, response)
         except Exception as ex:
             print(f"Failed to answer prompt {prompt.id}, exc: {ex}")
 
 
 async def _listen():
-    global calls_made
     while True:
         try:
-            _answer_unanswered_prompts()
+            await _answer_unanswered_prompts()
         except Exception as exc:
             print("Failed to index chain, exc:", exc)
-        asyncio.sleep(2)
-        print(f"Calls made: {calls_made}")
+        await asyncio.sleep(2)
+        print(f"Calls made: {repository.calls_made}")
 
 
 async def main():
--- oracles/src/repositories/oracle_repository.py
+++ oracles/src/repositories/oracle_repository.py
@@ -1,6 +1,6 @@
 import json
 from typing import List
-from web3 import Web3
+from web3 import AsyncWeb3
 from src.entities import PromptAndResponse
 
 import settings
@@ -9,10 +9,9 @@ import settings
 class OracleRepository:
 
     def __init__(self) -> None:
-        self.calls_made = 0
-        self.last_unindexed_prompt_id = -1
+        self.last_prompt_count = 0
         self.indexed_prompts = []
-        self.web3_client = Web3(Web3.HTTPProvider(settings.WEB3_RPC_URL))
+        self.web3_client = AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(settings.WEB3_RPC_URL))
         self.account = self.web3_client.eth.account.from_key(settings.PRIVATE_KEY)
         with open(settings.ORACLE_ABI_PATH, "r", encoding="utf-8") as f:
             oracle_abi = json.loads(f.read())["abi"]
@@ -21,94 +20,39 @@ class OracleRepository:
             address=settings.ORACLE_ADDRESS, abi=oracle_abi
         )
 
-    def _index_next_prompt(self):
-        current_prompt_index = self.last_unindexed_prompt_id + 1
-        while prompt := self._get_prompt(current_prompt_index):
-            self.calls_made = self.calls_made + 1
-            if prompt:
-                callback_address = self.contract.functions.callbackAddresses(
-                    current_prompt_index
-                ).call()
-                callback_id = self.contract.functions.promptCallbackIds(
-                    current_prompt_index
-                ).call()
-                is_prompt_processed = self.contract.functions.isPromptProcessed(
-                    current_prompt_index
-                ).call()
-                self.calls_made = self.calls_made + 3
-
-                if is_prompt_processed:
-                    print(
-                        f"Prompt with index {current_prompt_index} is already processed"
+    async def _index_new_prompts(self):
+        prompts_count = await self.oracle_contract.functions.promptsCount().call()
+        if prompts_count > self.last_prompt_count:
+            for i in range(self.last_prompt_count, prompts_count -1 ):
+                prompt = await self._get_prompt(i)
+                if prompt:
+                    callback_address = await self._get_callback_address(i)
+                    callback_id = await self.oracle_contract.functions.promptCallbackIds(i).call()
+                    is_prompt_processed = await self.oracle_contract.functions.isPromptProcessed(i).call()
+                    self.indexed_prompts.append(
+                        PromptAndResponse(
+                            id=i,
+                            prompt=prompt,
+                            callback_id=callback_id,
+                            callback_address=callback_address,
+                            is_processed=is_prompt_processed,
+                            response=None,
+                        )
                     )
-                else:
-                    print(f"Found new prompt: {prompt}")
 
-                self.indexed_prompts.append(
-                    PromptAndResponse(
-                        id=current_prompt_index,
-                        prompt=prompt,
-                        callback_id=callback_id,
-                        callback_address=callback_address,
-                        is_processed=is_prompt_processed,
-                        response=None,
-                    )
-                )
-                self.last_unindexed_prompt_id = current_prompt_index
-            current_prompt_index = self.last_unindexed_prompt_id + 1
-        else:
-            self.calls_made = self.calls_made + 1
-
-    def get_unanswered_prompts(self) -> List[PromptAndResponse]:
-        self._index_next_prompt()
-        return [prompt for prompt in self.indexed_prompts if not prompt.is_processed]
-
-    def _get_prompt(self, current_prompt_index: int) -> str:
+    async def _get_callback_address(self, current_prompt_index: int) -> str:
         try:
-            return self.contract.functions.prompts(current_prompt_index).call()
-        except:
+            return await self.oracle_contract.functions.callbackAddresses(current_prompt_index).call()
+        except Exception as e:
+            print(e)
             return None
 
-    def _index_next_prompt(self):
-        current_prompt_index = self.last_unindexed_prompt_id + 1
-        while prompt := self._get_prompt(current_prompt_index):
-            self.calls_made = self.calls_made + 1
-            if prompt:
-                callback_address = self.contract.functions.callbackAddresses(
-                    current_prompt_index
-                ).call()
-                callback_id = self.contract.functions.promptCallbackIds(
-                    current_prompt_index
-                ).call()
-                is_prompt_processed = self.contract.functions.isPromptProcessed(
-                    current_prompt_index
-                ).call()
-                calls_made = calls_made + 3
+    async def get_unanswered_prompts(self) -> List[PromptAndResponse]:
+        await self._index_next_prompt()
+        return [prompt for prompt in self.indexed_prompts if not prompt.is_processed]
 
-                if is_prompt_processed:
-                    print(
-                        f"Prompt with index {current_prompt_index} is already processed"
-                    )
-                else:
-                    print(f"Found new prompt: {prompt}")
-
-                self.indexed_prompts.append(
-                    PromptAndResponse(
-                        id=current_prompt_index,
-                        prompt=prompt,
-                        callback_id=callback_id,
-                        callback_address=callback_address,
-                        is_processed=is_prompt_processed,
-                        response=None,
-                    )
-                )
-                self.last_unindexed_prompt_id = current_prompt_index
-            current_prompt_index = self.last_unindexed_prompt_id + 1
-        else:
-            self.calls_made = self.calls_made + 1
-
-    def send_response(self, prompt: PromptAndResponse, response: str) -> bool:
-        nonce = self.web3_client.eth.get_transaction_count(self.account.address)
+    async def send_response(self, prompt: PromptAndResponse, response: str) -> bool:
+        nonce = await self.web3_client.eth.get_transaction_count(self.account.address)
         tx_data = {
             "from": self.account.address,
             "nonce": nonce,
@@ -119,15 +63,15 @@ class OracleRepository:
         }
         if chain_id := settings.CHAIN_ID:
             tx_data["chainId"] = int(chain_id)
-        tx = self.contract.functions.addResponse(
+        tx = await self.oracle_contract.functions.addResponse(
             prompt.id,
             response,
             prompt.callback_id,
         ).build_transaction(tx_data)
-        signed_tx = self.web3_client.eth.account.sign_transaction(
+        signed_tx = await self.web3_client.eth.account.sign_transaction(
             tx, private_key=self.account.key
         )
-        tx_hash = self.web3_client.eth.send_raw_transaction(signed_tx.rawTransaction)
-        tx_receipt = self.web3_client.eth.wait_for_transaction_receipt(tx_hash)
+        tx_hash = await self.web3_client.eth.send_raw_transaction(signed_tx.rawTransaction)
+        tx_receipt = await self.web3_client.eth.wait_for_transaction_receipt(tx_hash)
         prompt.is_processed = bool(tx_receipt.get("status"))
         return bool(tx_receipt.get("status"))
