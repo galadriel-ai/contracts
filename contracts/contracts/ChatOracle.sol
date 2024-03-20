0d4d6a1251d19d54ff25aa1800278e464d06e8cb
kristjanpeterson <kristjan@nftport.xyz>
2024-03-20 17:05:21 +0200

    POC support openAI api parameters etc

--- contracts/contracts/ChatOracle.sol
+++ contracts/contracts/ChatOracle.sol
@@ -4,6 +4,52 @@ pragma solidity ^0.8.13;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
+interface IOracleTypes {
+
+    struct OpenAiRequest {
+        // "gpt-4-turbo-preview" or "gpt-3.5-turbo-1106"
+        string model;
+        // int -20 - 20? Mapped to float -2.0 - 2.0? If bigger than 20 then null?
+        int8 frequencyPenalty;
+        string logitBias;
+        // 0 for null
+        uint32 maxTokens;
+        // int -20 - 20? Mapped to float -2.0 - 2.0? If bigger than 20 then null?
+        int8 presencePenalty;
+        // "" ,"text" or "json_object". In docs its actually '{"type": "json_object"}'
+        string responseFormat;
+        // 0 for null
+        uint seed;
+        // string / array / null - Up to 4 sequences where the API will stop generating further tokens.
+        string stop;
+        // 0-20 >20 for null
+        uint temperature;
+        // Not supported here? or list of str maybe of our supported tools?
+        // tools
+        string toolChoice;
+        string user;
+    }
+
+    struct OpenAiResponse {
+        string id;
+
+        // either content is an empty srt or functionName and functionArguments
+        string content;
+        string functionName;
+        string functionArguments;
+
+        uint64 created;
+        string model;
+        string systemFingerprint;
+        // kind of pointless since its always "chat.completion"?
+        string object;
+
+        uint32 completionTokens;
+        uint32 promptTokens;
+        uint32 totalTokens;
+    }
+}
+
 interface IChatGpt {
     function onOracleFunctionResponse(
         uint callbackId,
@@ -25,6 +71,12 @@ interface IChatGpt {
         uint callbackId
     ) external view returns (string[] memory);
 
+
+    function onOpenAiOracleLlmResponse(
+        uint callbackId,
+        IOracleTypes.OpenAiResponse memory response,
+        string memory errorMessage
+    ) external;
 }
 
 contract ChatOracle {
@@ -34,10 +86,24 @@ contract ChatOracle {
     mapping(address => string) public attestations;
     address public latestAttestationOwner;
 
+
+    // LLM calls
     mapping(uint => address) public callbackAddresses;
     mapping(uint => uint) public promptCallbackIds;
     mapping(uint => bool) public isPromptProcessed;
+    // "default", "OpenAI"
+    mapping(uint => string) public promptType;
+
+    mapping(uint => IOracleTypes.OpenAiRequest) public openAiConfigurations;
+
+
     uint public promptsCount;
+
+    event PromptAdded(
+        uint indexed promptId,
+        uint indexed promptCallbackId,
+        address sender
+    );
 
     mapping(uint => string) public functionInputs;
     mapping(uint => string) public functionTypes;
@@ -48,12 +114,6 @@ contract ChatOracle {
 
 
     address private owner;
-
-    event PromptAdded(
-        uint indexed promptId,
-        uint indexed promptCallbackId,
-        address sender
-    );
 
     event FunctionAdded(
         uint indexed functionId,
@@ -92,6 +152,7 @@ contract ChatOracle {
         callbackAddresses[promptId] = msg.sender;
         promptCallbackIds[promptId] = promptCallbackId;
         isPromptProcessed[promptId] = false;
+        promptType[promptId] = "default";
 
         promptsCount++;
 
@@ -159,6 +220,36 @@ contract ChatOracle {
         isFunctionProcessed[functionId] = true;
         IChatGpt(functionCallbackAddresses[functionId]).onOracleFunctionResponse(
             functionCallBackId,
+            response,
+            errorMessage
+        );
+    }
+
+    function createOpenAiLlmCall(uint promptCallbackId, IOracleTypes.OpenAiRequest memory config) public returns (uint i) {
+        uint promptId = promptsCount;
+        callbackAddresses[promptId] = msg.sender;
+        promptCallbackIds[promptId] = promptCallbackId;
+        isPromptProcessed[promptId] = false;
+        promptType[promptId] = "OpenAI";
+
+        promptsCount++;
+
+        openAiConfigurations[promptId] = config;
+        emit PromptAdded(promptId, promptCallbackId, msg.sender);
+
+        return promptId;
+    }
+
+    function addOpenAiResponse(
+        uint promptId,
+        uint promptCallBackId,
+        IOracleTypes.OpenAiResponse memory response,
+        string memory errorMessage
+    ) public onlyWhitelisted {
+        require(!isPromptProcessed[promptId], "Prompt already processed");
+        isPromptProcessed[promptId] = true;
+        IChatGpt(callbackAddresses[promptId]).onOpenAiOracleLlmResponse(
+            promptCallBackId,
             response,
             errorMessage
         );
