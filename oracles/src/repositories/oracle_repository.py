7539b53769608dbf69bb0194d60ed1e00cba1bbc
kristjanpeterson <kristjan@nftport.xyz>
2024-03-25 11:06:47 +0200

    Skip old chats and functions in oracle_repository.py

--- oracles/src/repositories/oracle_repository.py
+++ oracles/src/repositories/oracle_repository.py
@@ -11,9 +11,9 @@ import settings
 
 class OracleRepository:
     def __init__(self) -> None:
-        self.last_chats_count = 0
+        self.last_chats_count = 2700
         self.indexed_chats = []
-        self.last_function_calls_count = 0
+        self.last_function_calls_count = 1900
         self.indexed_function_calls = []
         self.web3_client = AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(settings.WEB3_RPC_URL))
         self.account = self.web3_client.eth.account.from_key(settings.PRIVATE_KEY)
