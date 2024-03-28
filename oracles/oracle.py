12838e9e050c422c7e99468511dcb0b098236187
kgrofelnik <kresimir.grofelnik@gmail.com>
2024-03-28 12:40:29 +0100

    Allows code interpreter tool usage

--- oracles/oracle.py
+++ oracles/oracle.py
@@ -90,7 +90,7 @@ async def _call_function(function_call: FunctionCall, semaphore: Semaphore):
                     error_message = web_search_result.error
                 elif function_call.function_type == "code_interpreter":
                     python_interpreter_result = await python_interpreter_use_case.execute(
-                        function_call.function_input
+                        formatted_input
                     )
                     response = python_interpreter_result.stdout
                     error_message = python_interpreter_result.stderr
