81d4f487d0ea8a4ba9ce4c6904c139e7f59a393c
Krešimir Grofelnik <kresimir.grofelnik@gmail.com>
2024-04-01 13:14:42 +0200

    testnet -> devnet (#50)

--- rag_tools/settings.py
+++ rag_tools/settings.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 CHAIN_ID = int(os.getenv("CHAIN_ID", "696969"))
-RPC_URL = os.getenv("RPC_URL", "https://testnet.galadriel.com")
+RPC_URL = os.getenv("RPC_URL", "https://devnet.galadriel.com")
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
 STORAGE_KEY = os.getenv("NFT_STORAGE_API_KEY")
 ORACLE_ADDRESS = os.getenv("ORACLE_ADDRESS")
