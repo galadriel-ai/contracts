3ccfe79763a9331d54f83b6eef1d9f9645fb1ee4
kgrofelnik <kresimir.grofelnik@gmail.com>
2024-04-02 12:48:37 +0200

    Adds chat with RAG deployment task

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,6 +1,7 @@
 import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "./tasks/whitelist";
+import "./tasks/deployChatWithRAG";
 
 require('dotenv').config()
 
