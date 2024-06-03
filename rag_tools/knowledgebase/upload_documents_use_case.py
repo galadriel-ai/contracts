bc119cf7e885b0c05efccc1dac10356cf05806f0
kgrofelnik <kresimir.grofelnik@gmail.com>
2024-06-03 12:51:36 +0200

    Switch RAG tools from nft.storage to pinata.cloud

--- rag_tools/knowledgebase/upload_documents_use_case.py
+++ rag_tools/knowledgebase/upload_documents_use_case.py
@@ -7,16 +7,17 @@ from langchain.schema import Document
 
 def execute(documents: List[Document]) -> str:
     serialized_data = _serialize_documents(documents)
+    multipart_data = {
+        "file": ("file", serialized_data, ),
+    }
     response = requests.post(
-        "https://api.nft.storage/upload",
+        "https://api.pinata.cloud/pinning/pinFileToIPFS",
         headers={
             "Authorization": f"Bearer {settings.STORAGE_KEY}",
-            "Content-Type": "text/plain",
         },
-        data=serialized_data,
+        files=multipart_data,
     )
-    response.raise_for_status()
-    return response.json().get("value").get("cid")
+    return response.json().get("IpfsHash")
 
 
 def _serialize_documents(documents: List[Document]) -> str:
